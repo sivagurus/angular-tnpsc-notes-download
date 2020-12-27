5b650691e5bf15ac48efba7bbd95df5efd93cd06
sivagurus <sivaguru1647@gmail.com>
2020-12-27 11:00:59 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+
+import {MatTreeModule} from '@angular/material/tree';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+
 import { RouterModule, Routes } from '@angular/router';
 import { VideoComponent } from './video/video.component';
 import { CourseComponent } from './course/course.component';
@@ -40,6 +43,7 @@ const routes: Routes = [
     MatSnackBarModule,
     MatToolbarModule,
     MatIconModule,
+    MatTreeModule,
     RouterModule.forRoot(routes)
   ],
   declarations: [ 
--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { ApiService } from './api.service';
+import * as _ from "lodash";
 
 @Component({
   selector: 'app-course',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseComponent implements OnInit {
 
-  constructor() { }
+  courses: any[] = [];
+
+  constructor(
+    private  apiService: ApiService
+  ) { }
 
   ngOnInit() {
+  }
+
+  getStore(){
+    this.apiService.getStore().pipe(
+      catchError((error) => {
+        return throwError(error)
+      })
+    ).subscribe((res: any) => {
+      this.courses = res.items;
+    });
   }
 
 }
